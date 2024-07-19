4197a1c416a6f657e9ab79a25fb4c2344c06461d
Saitorfer <zavidbohez@gmail.com>
2024-07-19 09:11:13 +0200

    Add slice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { RecipesSliceType, createRecipesSlice } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
 
 
 //the ...a take a copy of all the posible arguments, gets, sets, etc..
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
   ...createRecipesSlice(...a),
+  ...createFavoritesSlice(...a),
 })));
