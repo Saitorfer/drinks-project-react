f6a8b1a6e9d05122abf53a54d42632533e60f4fc
Saitorfer <zavidbohez@gmail.com>
2024-11-21 16:07:45 +0100

    update router
    improve router

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,38 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import IndexPage from './pages/IndexPage'
-import FavoritesPage from './pages/FavoritesPage'
-import Layout from './layouts/Layout'
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import IndexPage from "./pages/IndexPage";
+//import FavoritesPage from './pages/FavoritesPage'
+import Layout from "./layouts/Layout";
 
+//improvement for Routes pages
+const IndexPage = lazy(() => import("./pages/IndexPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
 export default function AppRouter() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route element={<Layout/>} >
-                <Route path='/' element={<IndexPage/>} index />
-                <Route path='/favorites' element={<FavoritesPage/>} />
-            </Route>
-        </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              //can use a loader animation here
+              <Suspense fallback="Charging">
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          />
+          <Route
+            path="/favorites"
+            element={
+              //can use a loader animation here
+              <Suspense fallback="Charging">
+                <FavoritesPage />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
